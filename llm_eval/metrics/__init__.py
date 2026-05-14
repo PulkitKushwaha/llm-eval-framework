cd6f98edd53b965df1a29a3cb2582a2a1ba5dc81
Pulkit Kushwaha <143074263+PulkitKushwaha@users.noreply.github.com>
2026-05-14 20:46:52 +0530

    feat: Register AnswerRelevancy in metrics module

--- llm_eval/metrics/__init__.py
+++ llm_eval/metrics/__init__.py
@@ -1,5 +1,6 @@
 # Metrics will be registered here as they are implemented
 
 from llm_eval.metrics.faithfulness import Faithfulness
+from llm_eval.metrics.answer_relevancy import AnswerRelevancy
  
-__all__ = ["Faithfulness"]
+__all__ = ["Faithfulness", "AnswerRelevancy"]
